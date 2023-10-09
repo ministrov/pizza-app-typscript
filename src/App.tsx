840a33be624b65a62f4c4c78e799070d37d1b44d
ministrov <61511030+ministrov@users.noreply.github.com>
2023-10-09 21:09:09 +0300

    feat: end up operating with hooks and events typing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
+import { useState, MouseEvent } from 'react';
 import Button from './components/Button/Button';
 import './App.css';
 
 function App() {
+  const [counter, setCounter] = useState<number>(0);
+
+  const addCounter = (event: MouseEvent) => {
+    console.log(event);
+    setCounter(counter + 1);
+  }
 
   return (
     <>
-      <Button onClick={() => console.log('Button is clicked!!!')}>Button</Button>
+      {counter}
+      <Button onClick={() => addCounter}>Button</Button>
     </>
   )
 }
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,6 +1,13 @@
+// import { FC } from 'react';
 import { ButtonProps } from './Button.props';
 import cn from 'classnames';
 import './Button.module.css';
+
+// export const ButtonAlt: FC<ButtonProps> = ({ children, className, ...props }: ButtonProps) => {
+//   return (
+//     <button className={cn('button accent', className)} {...props}>{children}</button>
+//   );
+// }
 
 function Button({ children, className, ...props }: ButtonProps) {
   return (
