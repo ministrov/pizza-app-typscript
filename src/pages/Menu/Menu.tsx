6de1f5c113a8817296531e3331186acb18ad0914
ministrov <61511030+ministrov@users.noreply.github.com>
2023-10-14 14:35:05 +0300

    feat: end up operating with creatin heading and search component

--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -1,11 +1,14 @@
 import Heading from '../../components/Headling/Heading';
+import Search from '../../components/Search/Search';
+import styles from './Menu.module.css';
 
 function Menu() {
   return (
-    <div>
+    <div className={styles.head}>
       <Heading>
         Меню
       </Heading>
+      <Search placeholder='Введите блюдо или состав'/>
     </div>
   );
 }
