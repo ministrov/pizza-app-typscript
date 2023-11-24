b21b5bc976f4b86d96af019e62679d432c4445a6
unknown <ministrov2018@gmail.com>
2023-11-24 11:17:22 +0300

    feat: start operating on creating cart state in store

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import userSlice, { JWT_PERSISTEN_KEY } from './user.slice';
 import counterSlice from './counter.slice';
 import { saveState } from './storage';
+import cartSlice from './cart.slice';
 
 
 export const store = configureStore({
   reducer: {
     user: userSlice,
+    cart: cartSlice,
     counter: counterSlice
   }
 });
@@ -14,6 +16,8 @@ export const store = configureStore({
 store.subscribe(() => {
   saveState({jwt: store.getState().user.jwt}, JWT_PERSISTEN_KEY);
 });
+
+store.subscribe(() => console.info(store.getState()));
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
