57d60392203977eb0c0fed25ab455fc5f5c7f56d
unknown <ministrov2018@gmail.com>
2023-11-29 11:26:03 +0300

    feat: start operating on ordering count

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -1,12 +1,15 @@
 import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import Heading from '../../components/Headling/Heading';
+import Button from '../../components/Button/Button';
+import CardItem from '../../components/CardItem/CardItem';
 import { Product } from '../../interfaces/product.interface';
 import { RootState } from '../../store/store';
 import axios from 'axios';
 import { PREFIX } from '../../helpers/API';
-import CardItem from '../../components/CardItem/CardItem';
 import styles from './Cart.module.css';
+
+const DELIVERY_FEE = 169;
 
 function Cart() {
   const [cartProducts, setCardProducts] = useState<Product[]>([]);
@@ -21,8 +24,6 @@ function Cart() {
 
     const loadAllItems = async () => {
       const result = await Promise.all(items.map(item => getItems(item.id)));
-
-      console.log(result);
 
       setCardProducts(result);
     };
@@ -43,6 +44,27 @@ function Cart() {
 
       return <CardItem key={item.id} count={item.count} {...product} />;
     })}
+
+    <div className={styles['promo-code']}>
+      <input className={styles['promo-code-field']} type='text' placeholder='Промокод' />
+      <Button appearence='small' children={'Применить'}/>
+    </div>
+
+    <div className={styles['ordering']}>
+      <div>
+        <div>Итог</div>
+        <div>{}</div>
+      </div>
+      <hr />
+      <div>
+        <div>Доставка</div>
+        <div>{DELIVERY_FEE}</div>
+      </div>
+      <hr />
+      <div>
+        <div>Итог {items.length}</div>
+      </div>
+    </div>
   </>;
 }
 
